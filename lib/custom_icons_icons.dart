03867810982cd14e8d64afbc927da665979288d0
Cihat Karaboga <hestu12@gmail.com>
2021-08-25 15:58:25 +0300

    Code refactoring

---
+++ lib/custom_icons_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String _kFontPkg = null;
+
+  static const IconData github_circled =
+      IconData(0xf09b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData twitter_squared =
+      IconData(0xf304, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData linkedin_squared =
+      IconData(0xf30c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
+import 'package:my_portfolio/custom_icons_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,102 +10,137 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment(0.8, 0.0),
-            colors: [const Color(0xff183940), const Color(0xff1B2B41)],
-          )),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 250,
-                height: 250,
-                child: CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Color(0xff183940),
-                  backgroundImage: NetworkImage(
-                      "https://scontent.fada1-8.fna.fbcdn.net/v/t1.0-9/71074570_2395748953793582_5236197842636242944_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=_uXjFBSsZB4AX8zgT9q&_nc_ht=scontent.fada1-8.fna&oh=d1d02862cbc6ff31549b7b9ebc2d547f&oe=600610B7"),
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Text(
-                "Cihat Karaboğa",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Softare Enginner Students. Flutter Developer.\nPart time Musician.",
-                style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Image.asset("icons8_github_32px.png"),
-                    onPressed: () {
-                      launch("https://github.com/Hestys");
-                    },
-                  ),
-                  IconButton(
-                    icon: Image.asset("icons8_linkedin_32px.png"),
-                    onPressed: () {
-                      launch("https://www.linkedin.com/in/chtkb/");
-                    },
-                  ),
-                  IconButton(
-                    icon: Image.asset("icons8_twitter_48px.png"),
-                    onPressed: () {
-                      launch("https://twitter.com/KarabogaCihat");
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.copyright,
-                    color: Colors.greenAccent,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "Created By Hesty with Flutter",
-                    style: TextStyle(color: Colors.greenAccent),
-                  ),
-                ],
-              )
-            ],
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment(0.8, 0.0),
+          colors: [const Color(0xff183940), const Color(0xff1B2B41)],
+        )),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: _buildBody(context),
           ),
+        ));
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * .1,
         ),
-      ),
+        _buildAvatar(),
+        SizedBox(
+          height: 25,
+        ),
+        _buildTitle(),
+        SizedBox(
+          height: 20,
+        ),
+        _buildSubtitle(),
+        SizedBox(
+          height: 20,
+        ),
+        _buildContactRow(),
+        //_buildCreatedTag()
+      ],
     );
   }
+
+  Widget _buildContactRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Color(0xff0474B3))),
+          child: Row(
+            children: [Icon(CustomIcons.linkedin_squared), Text('LinkedIn')],
+          ),
+          onPressed: () {
+            launch("https://www.linkedin.com/in/chtkb/");
+          },
+        ),
+        normalVerticalSpace,
+        ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Color(0xff252A2E))),
+          child: Row(
+            children: [Icon(CustomIcons.github_circled), Text('GitHub')],
+          ),
+          onPressed: () {
+            launch("https://github.com/hesty");
+          },
+        ),
+        normalVerticalSpace,
+        ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Color(0xff1EA1F1))),
+          child: Row(
+            children: [Icon(CustomIcons.twitter_squared), Text('Twitter')],
+          ),
+          onPressed: () {
+            launch("https://twitter.com/KarabogaCihat");
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget get normalVerticalSpace => SizedBox(
+        width: MediaQuery.of(context).size.width * .02,
+      );
+
+  Widget _buildAvatar() {
+    return Container(
+        width: 250,
+        height: 250,
+        child: CircleAvatar(
+            radius: 30, backgroundImage: AssetImage('avatar.png')));
+  }
+
+  Widget _buildTitle() {
+    return Text(
+      "Cihat Karaboğa",
+      style: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+    );
+  }
+
+  Widget _buildSubtitle() {
+    return Text(
+      "Softare Enginner Students. Flutter Developer.\nPart time Musician.",
+      style: TextStyle(
+        color: Colors.white70,
+        fontSize: 18,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  // Widget _buildCreatedTag() {
+  //   return Row(
+  //     crossAxisAlignment: CrossAxisAlignment.end,
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: [
+  //       Icon(
+  //         Icons.copyright,
+  //         color: Colors.greenAccent,
+  //       ),
+  //       SizedBox(
+  //         width: 10,
+  //       ),
+  //       Text(
+  //         "Created By Hesty with Flutter",
+  //         style: TextStyle(color: Colors.greenAccent),
+  //       ),
+  //     ],
+  //   );
+  // }
+
 }
